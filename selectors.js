5a88ad60c69d9a2133ba65d3678facb674a36486
Shruthi Devaraj <shruthi@founderandlightning.com>
2024-04-30 14:45:57 +0530

    Add files via upload

---
+++ selectors.js
@@ -0,0 +1,70 @@
+export const homePage = {
+    signUpButtonIndividual: '[data-cy="button-sign-up-individual"]',
+    signUpButtonTeam: '[data-cy="button-sign-up-team"]',
+    signInButton: '[data-cy="button-sign-in"]',
+    mediaFormButton: '[data-cy="button-media-form"]'
+  }
+
+  export const individualSignUp = {
+    firstName: '[data-cy="first-name"]',
+    lastName: '[data-cy="last-name"]',
+    email: '[data-cy="email"]',
+    businessName: '[data-cy="business-name"]',
+    password: '[data-cy="password"]',
+    reEnterPassword: '[data-cy="password-confirmation"]',
+    checkbox: '[data-cy="terms-and-conditions"]',
+    createAccountbtn: '[data-cy="submit-button"]',
+    
+  }
+
+  export const teamSignUp = {
+    firstName: '[data-cy="first-name"]',
+    lastName: '[data-cy="last-name"]',
+    email: '[data-cy="email"]',
+    businessName: '[data-cy="business-name"]',
+    password: '[data-cy="password"]',
+    reEnterPassword: '[data-cy="password-confirmation"]',
+    checkbox: '[data-cy="terms-and-conditions"]',
+    createAccountbtn: '[data-cy="submit-button"]',
+    
+  }
+
+  export const loginPage = {
+    createAccountLinkBtn: '[data-cy="create-an-account-link"]',
+    loginBtn: '[data-cy="submit-button"]',
+    resetPasswordLink: '[data-cy="reset-it-link"]',
+    journalistLink: '[data-cy="are-you-journalist-link"]',
+    email: '[data-cy="email"]',
+    password: '[data-cy="password"]',
+    
+  }
+
+  export const resetPassword = {
+    email: '[data-cy="email"]',
+    sendEmailBtn: '[data-cy="send-link"]',
+    resendEmailLink: '[data-cy="resend"]'
+  }
+
+  export const mediaFormPage = {
+    backBtn: '[data-cy="mf-back-button"]',
+    firstName: '#firstName',
+    lastName: '#lastName',
+    email: '[data-cy="mf-email"]',
+    nextBtn: '[data-cy="mf-submit-button"]',
+    nameOfPub: '[data-cy="mf-publication-name"]',
+    pubWebsite: '[data-cy="mf-publication-website"]',
+    mediatypeDropdown: '#react-select-4-placeholder',
+    iamDropdown: '#react-select-5-placeholder',
+    reqTitle: '#requestTitle',
+    details: '#details',
+    privacyChkbox: '#privacyPolicy',
+    secondChkbox: '#freeService'
+  }
+
+  export const logout = {
+    userProfileIcon: '[data-cy="userAvatar"]',
+    homeNavigation: '[data-cy="Home"]',
+    individualNavigation: '[data-cy="Individual"]',
+    helpNavigation: '[data-cy="Help"]',
+    logoutBtn: '[data-cy="Logout"]'
+  }
