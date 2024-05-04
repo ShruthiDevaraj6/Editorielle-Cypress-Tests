bad15e9091c53bb608a54879d24dc472a40db607
Shruthi Devaraj <shruthi@founderandlightning.com>
2024-05-04 15:39:34 +0530

    Update TC08_InviteTeamMembers.cy.js

--- TC08_InviteTeamMembers.cy.js
+++ TC08_InviteTeamMembers.cy.js
@@ -1,5 +1,4 @@
-const data = require ('C:/Users/Shruthi Devaraj/Cypress Automation Editorelle Project/cypress/fixtures/editorelle.json')
-
+import { faker } from '@faker-js/faker';
 import {homePage,teamSignUp} from "../support/selectors"
 import EmailGenerator from '../support/utils';
 describe('Invite team members functionality scenarios', () =>{
@@ -7,14 +6,13 @@ describe('Invite team members functionality scenarios', () =>{
     beforeEach(()=> {
         cy.visit("/")
         cy.get(homePage.signUpButtonTeam).click()
-        cy.fixture('editorelle.json').then((data)=>{
-         userdata=data
-        cy.get(teamSignUp.firstName).type(userdata.firstname)
-        cy.get(teamSignUp.lastName).type(userdata.lastname)
+         const password = faker.internet.password();
+        cy.get(teamSignUp.firstName).type(faker.person.firstName())
+        cy.get(teamSignUp.lastName).type(faker.person.lastName())
         cy.get(teamSignUp.email).type(EmailGenerator.generateRandomEmail())
-        cy.get(teamSignUp.businessName).type(userdata.businessName)
-        cy.get(teamSignUp.password).type(userdata.password)
-        cy.get(teamSignUp.reEnterPassword).type(userdata.password)
+        cy.get(teamSignUp.businessName).type(faker.company.name()) 
+        cy.get(teamSignUp.password).type(password)
+        cy.get(teamSignUp.reEnterPassword).type(password)
         cy.get(teamSignUp.checkbox).click()
         cy.get(teamSignUp.createAccountbtn).click()
         cy.url().should('include','/team/account/signup-verification')
@@ -26,34 +24,20 @@ describe('Invite team members functionality scenarios', () =>{
         cy.wait(5000)
         cy.get(teamSignUp.myselfBtn).click()
         cy.get(teamSignUp.businessNextBtn).click()
-        })
 })
 it('Should invite 6 maximum limit of team members using dynamic email',() =>{
-    cy.get(teamSignUp.firstName1).type(userdata.firstname1)
-    cy.get(teamSignUp.lastName1).type(userdata.lastname1)
-    cy.get(teamSignUp.memberEmail1).eq(0).type(EmailGenerator.generateRandomEmail())
-    cy.get(teamSignUp.addMemberBtn).click()
-    cy.get(teamSignUp.firstName2).type(userdata.firstname2)
-    cy.get(teamSignUp.lastName2).type(userdata.lastname2)
-    cy.get(teamSignUp.memberEmail2).eq(0).type(EmailGenerator.generateRandomEmail())
-    cy.get(teamSignUp.addMemberBtn).click()
-    cy.get(teamSignUp.firstName3).type(userdata.firstname3)
-    cy.get(teamSignUp.lastName3).type(userdata.lastname3)
-    cy.get(teamSignUp.memberEmail3).eq(0).type(EmailGenerator.generateRandomEmail())
-    cy.get(teamSignUp.addMemberBtn).click()
-    cy.get(teamSignUp.firstName4).type(userdata.firstname4)
-    cy.get(teamSignUp.lastName4).type(userdata.lastname4)
-    cy.get(teamSignUp.memberEmail4).eq(0).type(EmailGenerator.generateRandomEmail())
-    cy.get(teamSignUp.addMemberBtn).click()
-    cy.get(teamSignUp.firstName5).type(userdata.firstname5)
-    cy.get(teamSignUp.lastName5).type(userdata.lastname5)
-    cy.get(teamSignUp.memberEmail5).eq(0).type(EmailGenerator.generateRandomEmail())
-    cy.get(teamSignUp.addMemberBtn).click()
-    cy.get(teamSignUp.firstName6).type(userdata.firstname6)
-    cy.get(teamSignUp.lastName6).type(userdata.lastname6)
-    cy.get(teamSignUp.memberEmail6).eq(0).type(EmailGenerator.generateRandomEmail())
-    cy.get(teamSignUp.nextBtn).click()
-    cy.url().should('include','/team/choose-categories')
+    const numberOfMembers = 6;
+    for (let i = 1; i <= numberOfMembers; i++) {
+        const firstName = faker.person.firstName();
+        const lastName = faker.person.lastName();
+        const email = EmailGenerator.generateRandomEmail();
+        cy.get(teamSignUp[`firstName${i}`]).type(firstName);
+        cy.get(teamSignUp[`lastName${i}`]).type(lastName);
+        cy.get(teamSignUp[`memberEmail${i}`]).eq(0).type(email);
+        if (i < numberOfMembers) {
+            cy.get(teamSignUp.addMemberBtn).click();
+          }
+        }
 })
 it('Should display error message when invalid first name, last name and email are entered',() =>{
     cy.get(teamSignUp.firstName1).type('fn1@')
@@ -64,4 +48,35 @@ it('Should display error message when invalid first name, last name and email ar
     cy.contains("Please remove special characters or numbers in last name").should('be.visible')
     cy.contains("Please enter a valid email address").should('be.visible')
 })
+it('Should be able to remove the user invited upon clicking remove',() =>{
+    const numberOfMembers = 2;
+    for (let i = 1; i <= numberOfMembers; i++) {
+        const firstName = faker.person.firstName();
+        const lastName = faker.person.lastName();
+        const email = EmailGenerator.generateRandomEmail();
+        cy.get(teamSignUp[`firstName${i}`]).type(firstName);
+        cy.get(teamSignUp[`lastName${i}`]).type(lastName);
+        cy.get(teamSignUp[`memberEmail${i}`]).eq(0).type(email);
+        if (i < numberOfMembers) {
+            cy.get(teamSignUp.addMemberBtn).click();
+          }
+        }
+        cy.get(teamSignUp.removeBtn).click()
+    })
+    it.only('Should display error message when same email address is repeated while inviting member',() =>{
+        const numberOfMembers = 2;
+        const email = faker.internet.email();
+       for (let i = 1; i <= numberOfMembers; i++) {
+        const firstName = faker.person.firstName();
+        const lastName = faker.person.lastName();
+        cy.get(teamSignUp[`firstName${i}`]).type(firstName);
+        cy.get(teamSignUp[`lastName${i}`]).type(lastName);
+        cy.get(teamSignUp[`memberEmail${i}`]).type(email);
+        if (i < numberOfMembers) {
+            cy.get(teamSignUp.addMemberBtn).click();
+          }
+        }
+        cy.get(teamSignUp.nextBtn).click({multiple: true, force:true})
+        cy.get(teamSignUp.emailErrorMsg).scrollIntoView().should('be.visible',{force:true});
+    })
 })
